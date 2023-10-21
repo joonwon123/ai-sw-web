942fd56e6b02a37d83dfc958587f9acf5d7bd69c
joonwon123 <85498137+joonwon123@users.noreply.github.com>
2023-10-21 19:55:21 +0900

    Add files via upload

---
+++ ai_sw/date.js
@@ -0,0 +1,5 @@
+const date = new Date();
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+document.getElementById("date").innerHTML = year + "."+month+"."+day;
---
+++ ai_sw/main.js
@@ -0,0 +1,18 @@
+var goLunch = document.getElementById("goLunch");
+var goSug = document.getElementById("goSug");
+var lunchbox = document.getElementById("lunchbox");
+var suggestion = document.getElementById("suggestion");
+
+goLunch.addEventListener("click", navbar);
+
+function navbar() {
+  lunchbox.style.display = "block";
+  suggestion.style.display = "none";
+}
+
+goSug.addEventListener("click", navbar1);
+
+function navbar1() {
+  lunchbox.style.display = "none";
+  suggestion.style.display = "block";
+}
